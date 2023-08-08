b8794a3402ab9c1cbc75d5bdbbdf3e467bbc5834
lucas.r.manoguerra <kiu.manoguerra@gmail.com>
2023-08-08 15:38:41 -0300

    variant lenguage tech skills

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -10,11 +10,11 @@ interface SkillsProps {
 }
 
 export const Skills: FC<SkillsProps> = async ({ lang }) => {
-    const { dataQuest } = await getDictionary(lang)
+    const { dataQuest, techSkills } = await getDictionary(lang)
 
     return (
         <section className='py-52 flex  flex-col justify-center items-center gap-10'>
-            <h2 className='text-4xl font-bold text-blue-600 flex flex-row justify-start items-center gap-4 w-full'>Tech skills</h2>
+            <h2 className='text-4xl font-bold text-blue-600 flex flex-row justify-start items-center gap-4 w-full'>{techSkills.title}</h2>
             <div className='flex flex-row justify-center items-center w-full gap-4 flex-wrap'>
                 {listTechs.map((tech) => {
                     return <Techs key={tech.label} {...tech} />
