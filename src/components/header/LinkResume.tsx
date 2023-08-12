e74ebb613e50877a1b0f80d71d19060d87befb0b
lucas.r.manoguerra <kiu.manoguerra@gmail.com>
2023-08-12 15:03:31 -0300

    no ref more dependency sharp

--- src/components/header/LinkResume.tsx
+++ src/components/header/LinkResume.tsx
@@ -12,6 +12,7 @@ export const LinkResume: FC<LinkResumeProps> = ({ children, href }) => {
         <Link
             href={href}
             target='_blank'
+            rel='noopener noreferrer'
             className='text-primary group hover:text-white border border-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg w-40 py-2.5 text-center mr-2 mb-2 dark:border-primary dark:text-primary dark:hover:text-white dark:hover:bg-primary dark:focus:ring-blue-800 transition-colors duration-300 opacity-90'
         >
             <p className='flex flex-row justify-center items-center gap-2'>
