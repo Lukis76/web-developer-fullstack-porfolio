c2c1b6cd77445e9f29c3c9cd19529e6c8f2171dc
lucas.r.manoguerra <kiu.manoguerra@gmail.com>
2023-08-12 15:09:53 -0300

    z-index

--- src/components/navbarNavigation/NavbarNavigation.tsx
+++ src/components/navbarNavigation/NavbarNavigation.tsx
@@ -14,7 +14,7 @@ export const NavbarNavigation: FC<NavbarNavigationProps> = async ({ lang }) => {
   const { navigation  } = await getDictionary(lang)
 
   return (
-    <nav className='flex justify-center items-center bg-[rgba(255,255,255,0.6)] dark:bg-[rgba(0,0,0,0.6)] backdrop-blur-md sticky w-full z-20 top-0 border-b border-gray-200 dark:border-gray-700'>
+    <nav className='flex justify-center items-center bg-[rgba(255,255,255,0.6)] dark:bg-[rgba(0,0,0,0.6)] backdrop-blur-md sticky w-full -z-50 top-0 border-b border-gray-200 dark:border-gray-700'>
       <div className='max-w-screen-xl w-full flex flex-row items-center justify-between px-4 gap-4'>
         <a href='https://flowbite.com/' className='flex items-center justify-center gap-2'>
           <RocketMoon size={3.5} className='backdrop-filter drop-shadow-3xl' />
