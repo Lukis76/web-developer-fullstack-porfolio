92a209b6c42495cecec8be743a4c1edbe46f3774
lucas.r.manoguerra <kiu.manoguerra@gmail.com>
2023-08-14 13:20:00 -0300

    link init page

--- src/components/navbarNavigation/NavbarNavigation.tsx
+++ src/components/navbarNavigation/NavbarNavigation.tsx
@@ -5,6 +5,7 @@ import { SwithTheme } from './swithTheme'
 import { LiNavToSection } from '../ui/LiNavToSection'
 import { RocketMoon } from '../icons/RocketMoon'
 import { getDictionary } from '@/lib/dictionary'
+import Link from 'next/link'
 
 interface NavbarNavigationProps {
   lang: Locale
@@ -16,12 +17,12 @@ export const NavbarNavigation: FC<NavbarNavigationProps> = async ({ lang }) => {
   return (
     <nav className='flex justify-center items-center bg-[rgba(255,255,255,0.6)] dark:bg-[rgba(0,0,0,0.6)] backdrop-blur-md sticky w-full z-50 top-0 border-b border-gray-200 dark:border-gray-700'>
       <div className='max-w-screen-xl w-full flex flex-row items-center justify-between px-4 gap-4'>
-        <a href='https://flowbite.com/' className='flex items-center justify-center gap-2'>
+        <Link href={`/${lang}`} className='flex items-center justify-center gap-2'>
           <RocketMoon size={3.5} className='backdrop-filter drop-shadow-3xl' />
           <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>RocketDev.</span>
-        </a>
+        </Link>
         <div className=' flex flex-row items-center justify-end w-full px-4 text-xl '>
-          <ul className='flex p-4 md:p-0 mt-4 font-bold flex-row md:space-x-8 md:mt-0  dark:border-gray-700'>
+          <ul className='flex justify-center items-center p-4 md:p-0 mt-4 font-bold flex-row md:space-x-8 md:mt-0  dark:border-gray-700'>
             <li>
               <LiNavToSection section={'home'}>
                 {navigation.home}
@@ -42,6 +43,11 @@ export const NavbarNavigation: FC<NavbarNavigationProps> = async ({ lang }) => {
                 {navigation.Contact}
               </LiNavToSection>
             </li>
+            {/* <li>
+              <Link href={`/${lang}/blog`} className='flex px-6 h-12 items-center justify-center rounded-lg transition-colors bg-gray-100 border border-gray-300 hover:bg-gray-200  dark:bg-gray-700 dark:hover:bg-gray-600  text-primary dark:text-white font-extrabold dark:border-gray-600'>
+                {navigation.blog}
+              </Link>
+            </li> */}
           </ul>
         </div>
         <div className='flex flex-row justify-center items-center gap-2'>
