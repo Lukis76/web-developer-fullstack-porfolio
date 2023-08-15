2c18b4517fc827135ccf0d485fd040d2ccb02231
lucas.r.manoguerra <kiu.manoguerra@gmail.com>
2023-08-15 11:32:06 -0300

    prettier

--- src/components/card-post.tsx
+++ src/components/card-post.tsx
@@ -19,9 +19,19 @@ interface CardPostProps {
 
 export const CardPost: FC<CardPostProps> = (props) => {
     return (
-        <li className='max-w-5xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 hover:dark:bg-gray-700  duration-200 hover:scale-[1.01] transition-transform'>
+        <li className='max-w-5xl  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 hover:dark:bg-gray-700  duration-200 hover:scale-[1.01] transition-transform'>
             <Link href={`/en/blog/${props.slugAsParams}`} className='flex flex-row-reverse'>
-                {!!props.image && <Image className='rounded-none rounded-r-lg m-0' src={props.image} alt={props.title} height={300} width={300}  quality={50} loading='lazy'  />}
+                {!!props.image && (
+                    <Image
+                        className='rounded-none object-cover rounded-r-lg m-0'
+                        src={props.image}
+                        alt={props.title}
+                        height={300}
+                        width={300}
+                        quality={50}
+                        loading='lazy'
+                    />
+                )}
                 <div className='px-4 py-2'>
                     <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white truncate-two'>
                         Noteworthy technology acquisitions 2021
@@ -33,7 +43,9 @@ export const CardPost: FC<CardPostProps> = (props) => {
                     <p className='mb-3 font-normal text-gray-700 dark:text-gray-400 truncate-five'>
                         Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order. Lorem, ipsum dolor sit amet
                         consectetur adipisicing elit. Accusantium asperiores minima perspiciatis sunt eos, veritatis laudantium nisi excepturi exercitationem
-                        rerum tempora deserunt voluptatum consectetur? Minima iure temporibus ab dignissimos deleniti. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi reiciendis ut praesentium perspiciatis corporis qui minus optio aspernatur mollitia assumenda natus dicta hic inventore in temporibus, excepturi quos quaerat laborum!
+                        rerum tempora deserunt voluptatum consectetur? Minima iure temporibus ab dignissimos deleniti. Lorem ipsum dolor, sit amet consectetur
+                        adipisicing elit. Quasi reiciendis ut praesentium perspiciatis corporis qui minus optio aspernatur mollitia assumenda natus dicta hic
+                        inventore in temporibus, excepturi quos quaerat laborum!
                     </p>
                 </div>
             </Link>
