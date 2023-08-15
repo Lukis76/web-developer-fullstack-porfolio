8610d94b0b4999ae31ad6a8dc21bb0d17a9eb289
lucas.r.manoguerra <kiu.manoguerra@gmail.com>
2023-08-15 10:41:48 -0300

    test maping post

--- src/app/[lang]/blog/page.tsx
+++ src/app/[lang]/blog/page.tsx
@@ -1,34 +1,29 @@
 import { allPosts } from 'contentlayer/generated'
-import Image from 'next/image'
-import Link from 'next/link'
-import { notFound } from 'next/navigation'
+// import { notFound } from 'next/navigation'
+import { CardPost } from '@/components/card-post'
 import { FC } from 'react'
 interface BlogPageProps {}
 
-async function getPostFormParams(slug: string) {
-    const post = allPosts.find((post) => post.slugAsParams === slug)
+// async function getPostFormParams(slug: string) {
+//     const post = allPosts.find((post) => post.slugAsParams === slug)
 
-    if (!post) notFound()
-    return post
-}
+//     if (!post) notFound()
+//     return post
+// }
 
 const BlogPage: FC<BlogPageProps> = () => {
     return (
-        <div className='flex flex-col justify-start items-center py-10'>
-            <ul className='flex flex-col gap-2 justify-center items-center flex-wrap'>
+        <div className='flex flex-col justify-start items-center py-10 w-full'>
+            <ul className='flex flex-row gap-4 justify-center items-center flex-wrap w-full'>
                 {allPosts.map((post) => {
                     return (
-                        <li key={post.slug} className='border rounded-lg border-slate-300 dark:border-slate-700'>
-                            <Link href={`/blog/${post.slug.split('/')[2]}`} className='flex flex-row justify-center items-start'>
-                                <div className='flex flex-col justify-center items-start p-2'>
-                                    <h3>{post.title}</h3>
-                                    <span>{post.slug.split('/')[2]}</span>
-                                    <small>{post.date}</small>
-                                    <p>{post.description}</p>
-                                </div>
-                                {!!post?.image && <Image src={post.image} quality={50} width={200} height={200} alt={post.title} className='rounded-r-lg' />}
-                            </Link>
-                        </li>
+                        <>
+                            <CardPost key={1} {...post} />
+                            <CardPost key={2} {...post} />
+                            <CardPost key={3} {...post} />
+                            <CardPost key={4} {...post} />
+                            <CardPost key={5} {...post} />
+                        </>
                     )
                 })}
             </ul>
