b4ddca028be0941747dae496e158ec8a3cc83669
Lucas Manoguerra <kiu.manoguerra@gmail.com>
2023-08-22 14:27:37 -0300

    refactors

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,29 +1,11 @@
 import { NavbarNavigation } from '@/components/navbarNavigation'
 import { Locale, i18n } from '@/i18n.config'
-// import { Metadata } from 'next'
+import { Providers } from '@/providers/Providers'
 import { Inter } from 'next/font/google'
 import { FC, ReactNode } from 'react'
-import { Providers } from '@/porviders/Providers'
 import './global.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
-// export const metadata: Metadata = {
-//   icons: {
-//     icon: 'https://rocketdev.vercel.app/en/favicon.ico',
-//   },
-//   title: 'lucas manoguerra | web developer full stack',
-//   description:
-//     'Full stack developer with experience in | Javascript | Nextjs | Reactjs | Redux | Typescript | Nodejs | Express | GraphQL | Mongodb | PostgreSQL | Prisma | sequelize | Cypress | Jest | HTML | CSS | Figma.',
-//   openGraph: {
-//     title: 'lucas manoguerra | web developer full stack',
-//     description:
-//       'Full stack developer with experience in | Javascript | Nextjs | Reactjs | Redux | Typescript | Nodejs | Express | GraphQL | Mongodb | PostgreSQL | Prisma | sequelize | Cypress | Jest | HTML | CSS | Figma.',
-//     type: 'website',
-//     url: 'https://rocketdev.vercel.app',
-//     images: ['https://rocketdev.vercel.app/en/screen-porfolio.png'],
-//   },
-// }
 
 export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }))
